b7c6be95a6e7b555d96d60119f624b6cff29d0c4
Tobin Magle <tmagle@ip-10-0-0-245.us-east-2.compute.internal>
2021-02-24 20:32:34 +0000

    cleaned up reticulate demo

--- reticulate-demo.R
+++ reticulate-demo.R
@@ -5,22 +5,11 @@ sns<-import("seaborn")
 #import matplotlib.pyplot as plt
 plt<-import("matplotlib.pyplot")
 
-# ## The Data
-# 
-# The data set that we’ll be working with contains data about various characteristics of small animals from an ecological study.
-# * each row is an inidvidual animal trapped by field scientists
-# * each column has data about the trapped animals  like species, sex and weight. 
-
 # ### Loading the data
 
 surveys <- read_csv('data/complete_surveys.csv')
 
-
 # ### Scatterplot
-# 
-# To make a scatterplot, use `replot` and specify what dataset 
-#you're using (like with `distplot`) 
-#and what variables you want to plot on the `x` and `y` axes.
 
 #sns.relplot(data=surveys,
 sns$relplot(data = surveys,
